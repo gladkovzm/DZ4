2dd1df6f02f8641a7b670f92af2cc015617d1349
gladkov <gladkovzm@gmail.com>
2022-09-04 21:22:32 +0300

    Задача 25

--- Program.cs
+++ Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-s
+﻿//\\ Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+if (B == 0)
+{
+    Console.Write($"Число {A} в степени {B} = 1");
+}
+else
+{
+    int work = A;
+    for (int i = 2; i <= B; i++)
+    {
+        A *= work;
+    }
+    Console.WriteLine($"Число {work} в степени {B} = {A}");
+}
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
