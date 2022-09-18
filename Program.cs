a523e1daed7f1ae9e0ff8d1ee494d85062e59191
gladkov <gladkovzm@gmail.com>
2022-09-18 21:59:24 +0300

    задача 56

--- Program.cs
+++ Program.cs
@@ -287,35 +287,90 @@
 
 ////////////////////////DZ7
 
-// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//\\ Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Console.Clear();
+// Console.WriteLine("Неотсортированный массив: ");
+// int[,] array = new int[4, 4];
+// CreateArray(array);
+// WriteArray(array);
+
+// Console.WriteLine("Отсортированный массив: ");
+// OrderArrayLines(array);
+// WriteArray(array);
+
+// void OrderArrayLines(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       for (int k = 0; k < array.GetLength(1) - 1; k++)
+//       {
+//         if (array[i, k] < array[i, k + 1])
+//         {
+//           int temp = array[i, k + 1];
+//           array[i, k + 1] = array[i, k];
+//           array[i, k] = temp;
+//         }
+//       }
+//     }
+//   }
+// }
+
+// void CreateArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       array[i, j] = new Random().Next(10);
+//     }
+//   }
+// }
+
+// void WriteArray(int[,] array)
+// {
+//   for (int i = 0; i < array.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//       Console.Write(array[i, j] + " ");
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+
+//\\ Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
 Console.Clear();
-Console.WriteLine("Неотсортированный массив: ");
 int[,] array = new int[4, 4];
 CreateArray(array);
 WriteArray(array);
-
-Console.WriteLine("Отсортированный массив: ");
-OrderArrayLines(array);
-WriteArray(array);
-
-void OrderArrayLines(int[,] array)
+int minsum = 0;
+int sum = SumElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  int tempsum = SumElements(array, i);
+  if (sum > tempsum)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
-    {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
-      {
-        if (array[i, k] < array[i, k + 1])
-        {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
-        }
-      }
-    }
+    sum = tempsum;
+    minsum = i;
   }
+}
+
+Console.WriteLine($"\n{minsum+1} - строкa с наименьшей суммой элементов = {sum} ");
+
+
+int SumElements(int[,] array, int i)
+{
+  int sum = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sum += array[i,j];
+  }
+  return sum;
 }
 
 void CreateArray(int[,] array)
@@ -324,39 +379,23 @@ void CreateArray(int[,] array)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      array[i, j] = new Random().Next(10);
+      array[i, j] = new Random().Next(1, 10);
     }
   }
 }
 
-void WriteArray(int[,] array)
+void WriteArray (int[,] array)
 {
   for (int i = 0; i < array.GetLength(0); i++)
   {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-      Console.Write(array[i, j] + " ");
+      Console.Write(array[i,j] + " ");
     }
     Console.WriteLine();
   }
 }
-
-
-// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-
-// Например, задан массив:
-
-// 1 4 7 2
-
-// 5 9 2 3
-
-// 8 4 2 4
-
-// 5 2 6 7
-
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-
-// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//\\ Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, даны 2 матрицы:
 // 2 4 | 3 4
 // 3 2 | 3 3
@@ -364,14 +403,14 @@ void WriteArray(int[,] array)
 // 18 20
 // 15 18
 
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//\\ Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
 // 66(0,0,0) 25(0,1,0)
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+//\\ Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
 // 01 02 03 04
 // 12 13 14 05
